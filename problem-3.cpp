9e805ad0f719bb711eac8ab1af35ceda3ba8c371
Shakuntala <90212726+1Shakuntala@users.noreply.github.com>
2025-04-29 18:20:35 +0530

    problem-3.cpp

--- problem-3.cpp
+++ problem-3.cpp
@@ -1,45 +1,19 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main() {
-    double a, b;
-    string operation_type;
-
-    cout << "Enter the first number (a): ";
+    int a;
     cin >> a;
-    cout << "Enter the second number (b): ";
-    cin >> b;
-    cout << "Enter the operation (addition, subtraction, multiplication, division): ";
-    cin >> operation_type;
 
-    double result = 0;
+    int count = (a % 2 == 0) ? a - 1 : a;
 
-    if (operation_type == "addition") {
-        result = a + b;
-    }
-    else if (operation_type == "subtraction") {
-        result = a - b;
-    }
-    else if (operation_type == "multiplication") {
-        result = a * b;
-    }
-    else if (operation_type == "division") {
-        if (b != 0) {
-            result = a / b;
-        }
-        else {
-            cout << "Error: Division by zero is not allowed." << endl;
-            return 0;
+    for (int i = 0; i < count; i++) {
+        cout << 2 * i + 1;
+        if (i != count - 1) {
+            cout << ", ";
         }
     }
-    else {
-        cout << "Invalid operation type." << endl;
-        return 0;
-    }
 
-    cout << "The result of " << operation_type << " is: " << result << endl;
-
+    cout << endl;
     return 0;
 }
